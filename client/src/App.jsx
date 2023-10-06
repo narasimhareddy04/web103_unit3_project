65582798076e28ecded221ee81a08f163d30436d
Mohana Narasimha Reddy Attunuru <mattunur@asu.edu>
2023-10-05 22:50:29 -0700

    music events2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
   return (
     <div className="app">
       <header className="main-header">
-        <h1>UnityGrid Plaza</h1>
+        <h1>Eventful Explorer</h1>
 
         <div className="header-buttons">
           <Link to="/" role="button">
